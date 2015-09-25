565aef2a6abbe6d69930e7d90e10d15dd4c138f7
unknown <13645@HS222-01.vtsd.com>
2015-09-25 12:14:53 -0400

    message

---
+++ Chapter3Lab.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Chapter3Lab here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Chapter3Lab
+{
+    
+
+
+    /**
+     * Constructor for o
+     */
+    public static void main  Chapter3Lab()
+    {
+        /
+        system.otu.p
+    }
+
+   
---
+++ Lab03Dst.java
@@ -0,0 +1,56 @@
+// Lab03Dst.java
+// The Visible Average Program
+// This is the student, starting version, of Lab 03C.
+
+
+public class Lab03Dst
+{
+    public static void main(String args[])
+    {
+        System.out.println("Lab 03D");
+        System.out.println();
+        int Q = 0; 
+        int a0 = 0;
+        int a1 = 11;
+        int a2 = 22;
+        int a3 = 33;
+        int a4 = 44;
+        int a5 = 55;
+        int S = 165; // Sum of all
+        int A = 5 ;  // Average
+        a0 = a1 + a2 + a3 + a4 + a5;
+        Q = S / A;
+        System.out.println("1st interger:"+a1);
+        System.out.println("2nd interger:"+a2);
+        System.out.println("3rd interger:"+a3);
+        System.out.println("4th interger:"+a4);
+        System.out.println("5th interger:"+a5);
+        System.out.println("----------------");
+        System.out.println("Sum total:" + a0); 
+        System.out.println("Average" + S / A); 
+        System.out.println();
+        double r1 = 11.11;
+        double r2 = 22.22;
+        double r3 = 33.33;
+        double r4 = 44.44;
+        double r5 = 55.55; 
+        double Sum = r1 + r2 + r3 + r4 + r5;
+        double Average = (r1 + r2 + r3 + r4 + r5)/5;
+        System.out.println("----------------"); 
+        System.out.println("1st real num:" + r1);
+        System.out.println("2nd real num:" + r2);
+        System.out.println("3rd real num:" + r3);
+        System.out.println("4th real num:" + r4);
+        System.out.println("5th real num:" + r5);
+        System.out.println("----------------");
+        System.out.println("Real total:" + Sum);
+        System.out.println("Real Average:" + Average); 
+        
+
+
+
+
+
+        System.out.println();
+    }
+}
